55e0ee2cfb672720f2c10ea00ef7b4aebd07304f
Loetheri <65231940+Loetheri@users.noreply.github.com>
2020-05-29 18:57:08 +0200

    Add requirements.txt and ImmoWeb.py
    ImmoWeb.py have to be reworked

--- ImmoWeb.py
+++ ImmoWeb.py
@@ -1,126 +1,165 @@
 # -*- coding: utf8 -*-
-from bs4 import BeautifulSoup
-import requests
+# Standard library imports
+import json
+import re
+import pathlib
 import math
 import time
+import datetime
+
+# Third party imports
+from bs4 import BeautifulSoup
+import requests
 
 
-REQUESTS_HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0'}
-SEARCHPAGE_IMMOWEB = "https://www.immoweb.be/fr/recherche/"
+def set_config_from_json(file_json):
+    # Retrieve search configuration from config.json
+    with open(file_json) as json_file:
+        data = json.load(json_file)
+    return data
 
-Rent = True # If Rent = True, Louer sinon Acheter
-Apartement = True # If Appartement = True, Appartement else Maison
-PostalCode = [1090,1083,1070,1020,1081,1080] # Verify if PostalCode exists in Belgium
-MinSurf = 50
-MaxSurf = 80
+def set_config_sub_types_mapping_table(config="config/config.json", sub_types="config/sub_types.json", classified_map_table="config/classified_map_table.json"):
+    return set_config_from_json(config), set_config_from_json(sub_types), set_config_from_json(classified_map_table)
 
-
-# Retrieve Search Page from ImmoWeb in Belgium.
-# Standard, search is on rent apartement. 
-def serve_search_page(PostalCodes, PageNbr=1, Apartement=True, Rent=True,MinPrice=0, MaxPrice=0, MinRoom=0, MaxRoom=0,MinSurf=0,MaxSurf=0):
-    SearchPage = [SEARCHPAGE_IMMOWEB]
-    SearchPage.append("appartement/" if Apartement else "maison/")
-    SearchPage.append("a-louer?orderBy=newest&countries=BE" if Rent else "a-vendre?orderBy=newest&countries=BE")
-    SearchPage.append("&page="+str(PageNbr))
-    if isinstance(MinPrice,int) and MinPrice > 0:
-        SearchPage.append("&minPrice="+str(MinPrice))
-    if isinstance(MaxPrice,int) and MaxPrice >0: #Verify if MaxPrice > MinPrice is needed
-        SearchPage.append("&maxPrice="+str(MaxPrice))
-    if isinstance(MinRoom,int) and MinRoom > 0:
-        SearchPage.append("&minBedroomCount="+str(MinRoom))
-    if isinstance(MaxRoom,int) and MaxRoom > 0: #Verify if MaxRoom > MinRoom is needed
-        SearchPage.append("&maxBedroomCount="+str(MaxRoom))
-    if isinstance(MinSurf,int) and MinSurf > 0:
-        SearchPage.append("&minSurface="+str(MinSurf))
-    if isinstance(MaxSurf,int) and MaxSurf > 0: #Verify if MaxRoom > MinRoom is needed
-        SearchPage.append("&maxSurface="+str(MaxSurf))
-    PostalCodes.sort()
-    SearchPage.append("&postalCodes=BE-"+"%2C".join(str(x) for x in PostalCodes))
+def prepare_search_page_url(page_nbr):
+    # Retrieve Search Page from ImmoWeb in Belgium.
+    # Standard, search is on rent apartement.
+    search_page = [CONFIG['SEARCHPAGE_IMMOWEB']]
     
-    return ''.join(SearchPage)
+    search_page.append("appartement/" if CONFIG['apartement'] else "maison/")
+    search_page.append("a-louer?orderBy=newest&countries=BE" if CONFIG['rent'] else "a-vendre?orderBy=newest&countries=BE")
+    search_page.append("&page="+str(page_nbr))
+    
+    # Add criteria (min & max) on Price/Surface/Room
+    if isinstance(CONFIG['min_price'],int) and CONFIG['min_price'] > 0:
+        search_page.append("&minPrice="+str(CONFIG['min_price']))
+    if isinstance(CONFIG['max_price'],int) and CONFIG['max_price'] >0: #Verify if max_price > min_price is needed
+        search_page.append("&maxPrice="+str(CONFIG['max_price']))
+    if isinstance(CONFIG['min_room'],int) and CONFIG['min_room'] > 0:
+        search_page.append("&minBedroomCount="+str(CONFIG['min_room']))
+    if isinstance(CONFIG['max_room'],int) and CONFIG['max_room'] > 0: #Verify if max_room > min_room is needed
+        search_page.append("&maxBedroomCount="+str(CONFIG['max_room']))
+    if isinstance(CONFIG['min_surf'],int) and CONFIG['min_surf'] > 0:
+        search_page.append("&minSurface="+str(CONFIG['min_surf']))
+    if isinstance(CONFIG['max_surf'],int) and CONFIG['max_surf'] > 0: #Verify if max_surf > min_surf is needed
+        search_page.append("&maxSurface="+str(CONFIG['max_surf']))
+    
+    # Add all postal codes
+    CONFIG['postal_code'].sort()
+    search_page.append("&postalCodes=BE-"+"%2C".join(str(x) for x in CONFIG['postal_code']))
+    
+    return ''.join(search_page)
 
-print(serve_search_page([1090,1070,1083],MinSurf=MinSurf,MaxSurf=MaxSurf))
+def prepare_classified_page_url(classified):
+    # Return the url of a classified
+    property_type = SUB_TYPES[classified["property"]["subtype"]] + "/"
+    transaction = "a-louer/" if classified["transaction"]["type"] == "FOR_RENT" else "a-vendre/"
+    
+    return CONFIG['IMMOWEB_CLASSIFIED'] + property_type + transaction + classified["property"]["location"]["locality"].lower() + "/" + classified["property"]["location"]["postalCode"] + "/" + str(classified["id"])
 
-# Retrieve saved BCE page and prepare it for parsin
-def serve_saved_page_soup(file):
-    file_bce = open(file,'r')
-    if file_bce.mode == 'r':
-        contents = file_bce.read()
-        file_bce.close()
-    return BeautifulSoup(contents.encode('latin-1').decode('utf-8'), 'html.parser')
-
-def verify_existence_company(soup):
-    print(soup.h1)
-    if soup.h1.string == None:
-        return False
-    elif soup.h1.string == "Données de l'entité enregistrée":
-        return True
-    elif soup.h1.string.find("n'existe pas dans la BCE") != -1:
-        return False
+def prepare_search_page_soup(nbr_page):
+    # Request the first searchPage and take all the Immoweb Code on this page
+    if not TEST:
+        search_page = requests.get(prepare_search_page_url(nbr_page),headers=CONFIG['REQUESTS_HEADERS'])
+        search_soup = BeautifulSoup(search_page.content, "html.parser")
+        
+        if SAVE :
+            with open("test/search_soup.html", "w", encoding="utf-8") as classified_file:
+                classified_file.write(search_soup.prettify())
     else:
-        print(soup)
-        raise
+        with open('test/search_soup.html','r', encoding="utf-8") as search_file :
+            search_page = search_file.read()
+        search_soup = BeautifulSoup(search_page,"html.parser")        
+    return search_soup
 
-def retrieve_info_company(soup):
-    # Retrieve info if company exists
-    if verify_existence_company(soup) == True:
-        data = soup.div.table
-        #print(data)
-        for td in data.find_all('td'):
-            if td.string == None:
-                continue
-            if td.string.find("Statut") != -1:
-                status = td.next_sibling.span.string
-            elif td.string.find("Situation") != -1:
-                legal_situation = td.next_sibling.contents[0].text
-                # Date à retravailler avec "Depuis le"
-                legal_situation_date = td.next_sibling.contents[2].text
-            elif td.string.find("Date de début") != -1:
-                # Date à retravailler
-                start_date = td.next_sibling.contents[0]
-            elif td.string.find("Dénomination") != -1:
-                name = td.next_sibling.contents[0]
-                # Date à retravailler avec "depuis le"
-                name_date = td.next_sibling.contents[2].text.split(",")[1]
+def split_take_first(str_to_split, splitting_str):
+    # split a string using splitting_str and return the first element
+    return str_to_split.split(splitting_str)[0]
 
-            elif td.string.find("Adresse du siège") != -1:
-                address_street = readable_text(td.next_sibling.contents[0])
-                address_city = readable_text(td.next_sibling.contents[3])
-                address_country = readable_text(td.next_sibling.contents[5])
-                # Date à retravailler avec "Depuis le"
-                address_date = td.next_sibling.contents[6].text
-            elif td.string.find("téléphone") != -1:
-                phone = td.next_sibling.contents[0]
-            elif td.string.find("mail") != -1:
-                email = td.next_sibling.contents
-            elif td.string.find("entité") != -1:
-                entity_type = td.next_sibling.contents[0]
-            elif td.string.find("Forme légale") != -1:
-                legal_form = readable_text(td.next_sibling.contents[0])
-            elif td.string.find("Fonctions") != -1:
-                print(td.previous_element.next_sibling.contents[0].contents[0])
-                # A verifier
-                functions = []
-            elif td.string.find("") != -1:
-                professional_skills = []
-            elif td.string.find("") != -1:
-                Licences = []
-            elif td.string.find("") != -1:
-                VAT_activities = []
-                
-            
+def format_new_classified(classified):
+    # replace str in int for integer values
+    classified["floor"] = int(classified["floor"]) if "floor" in classified.keys() else 0
+    classified["bedrooms"] = int(classified["bedrooms"]) if "bedrooms" in classified.keys() else 0
+    classified["bathrooms"] = int(classified["bathrooms"]) if "bathrooms" in classified.keys() else 0
+    
+    classified["surf"] = int(split_take_first(classified["surf"],"m²")) if "surf" in classified.keys() else 0
+    classified["terrasse"] = int(split_take_first(classified["terrasse"],"m²")) if "terrasse" in classified.keys() else 0
+    classified["loyer"] = int(split_take_first(classified["loyer"],"€").replace(".","")) if "loyer" in classified.keys() else 0
+    classified["charge"] = int(split_take_first(classified["charge"],"€")) if "charge" in classified.keys() else 0
+    classified["etat"] = classified["etat"].encode("utf-8").decode("utf-8") if "etat" in classified.keys() else ""
+    return classified
 
-# for i in range(100):
-#     bce_number = calculate_next_bce_number(bce_number)
-#     print(bce_number)
-#     
-#     soup = serve_bce_page_soup(bce_number)
-#     #print(soup)
-#     if verify_existence_company(soup) == True:
-#         f = open("bce_number.txt","a+")
-#         f.write(bce_number)
-#         f.close()
-#         f2 = open(bce_number+".html","w+")
-#         f2.write(soup.prettify())
-#         f2.close()
-#     time.sleep(30)
+def create_new_classified(soup):
+    classified_id = str(soup.find("div",class_="classified__information--immoweb-code").string.split(":")[1].strip())
+    
+    new_classified = {"first_seen": str(datetime.date.today()), "last_seen": str(datetime.date.today())}
+    for desktop6 in soup.find_all(class_="desktop--6") :
+        for key, values in zip(desktop6.find_all("th"),desktop6.find_all("td")):
+            if key.text.strip() in CLASSIFIED_MAP_TABLE:
+                new_classified[CLASSIFIED_MAP_TABLE[key.text.strip()]] = " ".join(values.text.split())
+    
+     # Save the photos in a folder named by the id of the classified
+    if CONFIG['photos']:
+        # create the folder
+        if not pathlib.Path("classifieds/" + classified_id).is_dir():
+            pathlib.Path("classifieds/" + classified_id).mkdir(parents=True, exist_ok=True)
+        # save pictures if program can reach the pictures
+        for picture in json.loads(str(classified_soup.find("script", string=re.compile("{\"pictures\":")).string.split("{\"pictures\":")[1].split(",\"virtualTourUrl\":null")[0])):
+            picture_request = requests.get(picture["largeUrl"],headers=CONFIG['REQUESTS_HEADERS'])
+            if picture_request.status_code == 200:    
+                with open("classifieds/" + classified_id+"/"+picture["largeUrl"].split("/")[-1].split("?")[0], "wb") as f:
+                    f.write(picture_request.content)
+    
+    return format_new_classified(new_classified)
+
+if __name__ == "__main__":
+    
+    global TEST, SAVE
+    TEST = False # True if we are in testing
+    SAVE = False
+    
+    # Load configuration files
+    global CONFIG, SUB_TYPES, CLASSIFIED_MAP_TABLE
+    CONFIG, SUB_TYPES, CLASSIFIED_MAP_TABLE = set_config_sub_types_mapping_table()
+    # Load saved classifieds
+    if pathlib.Path("saved_classified.json").exists():
+        with open("saved_classified.json", "r") as f:
+            saved_classified = json.load(f)
+    else:
+        saved_classified = {}
+    
+    search_soup = prepare_search_page_soup(1)
+    total_pages = math.ceil(int(str(search_soup.find('iw-search')).split("result-count=\"")[1].split("\"")[0])/30)
+        
+    for i in range(total_pages):
+        print("*** PAGE :"+str(i+1)+" / "+ total_pages)
+        search_soup = prepare_search_page_soup(i+1)
+        classifieds = json.loads(str(search_soup.find('iw-search')).split("results-storage='")[1].split("' :unique-id")[0])
+        
+        # Cross ImmoWebCodes to find the new classifieds or update open classifieds
+        # If classifieds don't exist no more, nothing to do
+        for classified in classifieds:
+            if str(classified["id"]) in saved_classified.keys() :
+                print(str(classified["id"]) + " already exists")
+                saved_classified[str(classified["id"])]["last_seen"] = str(datetime.date.today())
+            else:
+                print(str(classified["id"]) + " doesn't exist")
+                if not TEST:
+                    classified_page = requests.get(prepare_classified_page_url(classified),headers=CONFIG['REQUESTS_HEADERS'])
+                    classified_soup = BeautifulSoup(classified_page.content,"html.parser")
+                    
+                    if SAVE : 
+                        with open("classifieds/" + str(classified["id"])+".html", "w", encoding="utf-8") as classified_file:
+                            classified_file.write(classified_soup.prettify())
+                    time.sleep(CONFIG["stop_tim"])
+                else:
+                    with open("classifieds/" + str(classified["id"])+".html",'r', encoding="utf-8") as classified_file :
+                        classified_page = classified_file.read()
+                    classified_soup = BeautifulSoup(classified_page,"html.parser")
+             
+                saved_classified[classified['id']] = create_new_classified(classified_soup)
+    
+    with open('saved_classified.json', 'w') as fp:
+        json.dump(saved_classified, fp,ensure_ascii=False)
+#     print(json.dumps(saved_classified,indent=4,ensure_ascii=False))
+#     print(str(datetime.date.today()))
